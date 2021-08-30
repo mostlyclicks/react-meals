a37b88a8a609f37497fad9f20cb9263e635e5d09
Carlos Interone <carlos@mostlyclicks.com>
2021-08-30 14:50:50 -0600

    clean meal item divs/form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,26 +1,19 @@
-import React from 'react'
-import classes from './MealItem.module.css'
+import React from "react";
+import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
+	const price = `$${props.price.toFixed(2)}`;
 
-  const price = `$${props.price}`
+	return (
+		<li className={classes.meal}>
+			<div>
+				<h3>{props.name}</h3>
+				<div className={classes.description}>{props.description}</div>
+				<div className={classes.price}>{price}</div>
+			</div>
+			<div>form</div>
+		</li>
+	);
+};
 
-  return (
-    <li className={classes.meal}>
-      <div>
-        <h3>{props.name}</h3>
-      <div className={classes.description}>
-        {props.description}
-      </div>
-      <div className={classes.price}>
-        {price}
-      </div>
-      <div>
-        form
-      </div>
-      </div>
-    </li>
-  )
-}
-
-export default MealItem
+export default MealItem;
