adfcb0cf7e81fc6d657c82076b13ecd61bdccef2
Carlos Interone <carlos@mostlyclicks.com>
2021-08-31 19:38:21 -0600

    add form + form input components

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import classes from './MealItemForm.module.css'
+import Input from '../../UI/Input'
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
-    <div>
+    <form className={classes.form}>
+      <Input label="Amount" input={{
+        id: 'amount',
+        type: 'number',
+        min: '1',
+        max: '5',
+        step: '1',
+        defaultValue: '1'
+      }}/>
+      <button className={classes.button}>+ Add</button>
       
-    </div>
+    </form>
   )
 }
 
